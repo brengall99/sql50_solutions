d688c4768ae01eb163883464accbf55e339f1d2e
Brendan Gallagher <159880330+brengall99@users.noreply.github.com>
2024-06-19 13:33:50 +0100

    Add files via upload

---
+++ Advanced Selects and Joins/610-triangle-judgement.sql
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/triangle-judgement/description/
+
+SELECT *,
+    IF((
+        (x + y) > z AND
+        (y + z) > x AND
+        (z + x) > y), "Yes", "No") AS triangle
+FROM
+    Triangle;
