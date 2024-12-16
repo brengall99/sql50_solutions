8e72ede3d897b578c388f8021b1721232767c8f1
Brendan Gallagher <159880330+brengall99@users.noreply.github.com>
2024-12-16 11:55:20 +0000

    Update 595-big-countries.py

--- python/select/595-big-countries.py
+++ python/select/595-big-countries.py
@@ -5,7 +5,7 @@ import pandas as pd
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
 
     df_result = world[
-        (world.population >= 25000000) | (world.area >= 3000000)
+        (world['population'] >= 25000000) | (world['area'] >= 3000000)
     ]
 
     return df_result[['name', 'population', 'area']]
