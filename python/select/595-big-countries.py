dc87bf9815a1d792617c9089e8a236ce957f11e4
Brendan Gallagher <159880330+brengall99@users.noreply.github.com>
2024-12-16 11:51:16 +0000

    Add files via upload

---
+++ python/select/595-big-countries.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/big-countries/description/
+
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+
+    df_result = world[
+        (world.population >= 25000000) | (world.area >= 3000000)
+    ]
+
+    return df_result[['name', 'population', 'area']]
